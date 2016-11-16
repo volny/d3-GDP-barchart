3afde0968535b8481b86c5aaa8005ae5d21a7e16
felix <felix@volny.co>
2016-11-16 14:51:07 +0700

    parsing dates from fcc dataset correctly

--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
 import './style.scss'
-import { scaleLinear, scaleTime, timeParse, line, select, csv, extent, max, axisBottom, axisLeft } from 'd3'
+import { scaleLinear, scaleTime, timeParse, line, select, csv, extent, max, axisBottom, axisLeft, json } from 'd3'
 
 const API_URL = 'https://gist.githubusercontent.com/d3noob/402dd382a51a4f6eea487f9a35566de0/raw/6369502941b44261f381399a24fb455cb4290be8/data.csv'
+
+const FCC_URL = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
 const
   margin = {top: 20, right: 20, bottom: 30, left: 50},
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom
 
-const parseTime = timeParse("%d-%b-%y")
+//const parseTime = timeParse("%d-%b-%y")
+const parseTime = timeParse("%Y-%m-%d")
 
 const
   x = scaleTime().range([0, width]),
@@ -26,30 +29,41 @@ const svg = select('#app')
   .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 // GET DATA
-csv(API_URL, (error, data) => {
+//csv(API_URL, (error, data) => {
+json(FCC_URL, (error, response) => {
+
   if (error) throw error
 
+  const data = response.data
+
   data.forEach((row) => {
-    row.date = parseTime(row.date)
-    row.close = +row.close
+    console.log(
+    row[0] = parseTime(row[0]),
+    row[1] = +row[1]
+    )
   })
 
-  // scale the range of the data ???
-  x.domain(extent(data, (row) => row.date))
-  y.domain([0, max(data, (row) => row.close)])
-
-  // draw the path
-  svg.append('path')
-    .data([data])
-    .attr('class', 'line')
-    .attr('d', valueline)
-
-  // Add the Axes
-  svg.append('g')
-    .attr('transform', `translate(0, ${height})`)
-    .call(axisBottom(x));
-  svg.append('g')
-    .call(axisLeft(y));
+//  data.forEach((row) => {
+//    row.date = parseTime(row.date)
+//    row.close = +row.close
+//  })
+//
+//  // scale the range of the data ???
+//  x.domain(extent(data, (row) => row.date))
+//  y.domain([0, max(data, (row) => row.close)])
+//
+//  // draw the path
+//  svg.append('path')
+//    .data([data])
+//    .attr('class', 'line')
+//    .attr('d', valueline)
+//
+//  // Add the Axes
+//  svg.append('g')
+//    .attr('transform', `translate(0, ${height})`)
+//    .call(axisBottom(x));
+//  svg.append('g')
+//    .call(axisLeft(y));
 })
 
 
