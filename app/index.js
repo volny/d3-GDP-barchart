a96857af470fd365b69013905f8250086d5032e6
felix <felix@volny.co>
2016-11-16 20:31:22 +0700

    styling and code cleanup

--- app/index.js
+++ app/index.js
@@ -1,29 +1,12 @@
 import './style.scss'
-import { scaleLinear, scaleTime, timeParse, line, select, csv, extent, max, axisBottom, axisLeft, json } from 'd3'
-
-const API_URL = 'https://gist.githubusercontent.com/d3noob/402dd382a51a4f6eea487f9a35566de0/raw/6369502941b44261f381399a24fb455cb4290be8/data.csv'
+import { scaleLinear, scaleTime, timeParse, line, select, json, extent, max, axisBottom, axisLeft } from 'd3'
 
 const FCC_URL = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
 
 const
-  margin = {top: 20, right: 20, bottom: 30, left: 50},
+  margin = {top: 20, right: 20, bottom: 30, left: 60},
   width = 960 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom
-
-//const parseTime = timeParse("%d-%b-%y")
-const parseTime = timeParse("%Y-%m-%d")
-
-const
-  x = scaleTime().range([0, width]),
-  y = scaleLinear().range([height, 0])
-
-//const valueline = line()
-//  .x((row) => x(row.date))
-//  .y((row) => y(row.close))
-
-const valueline = line()
-  .x((row) => x(row[0]))
-  .y((row) => y(row[1]))
 
 const svg = select('#app')
   .append('svg')
@@ -33,8 +16,16 @@ const svg = select('#app')
   .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 json(FCC_URL, (error, response) => {
-  if (error) throw error
   const data = response.data
+  const parseTime = timeParse("%Y-%m-%d")
+  const
+    x = scaleTime().range([0, width]),
+    y = scaleLinear().range([height, 0])
+  const valueline = line()
+   .x((row) => x(row[0]))
+   .y((row) => y(row[1]))
+
+  if (error) throw error
 
   data.forEach((row) => {
     row[0] = parseTime(row[0])
@@ -50,11 +41,12 @@ json(FCC_URL, (error, response) => {
     .attr('d', valueline)
 
   svg.append('g')
+    .style('font', '15px Helvetica')
     .attr('transform', `translate(0, ${height})`)
     .call(axisBottom(x))
 
   svg.append('g')
+    .style('font', '15px Helvetica')
     .call(axisLeft(y))
-
 })
 
